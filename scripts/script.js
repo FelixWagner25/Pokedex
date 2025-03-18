5c6687a5601ea85f17259c5b4cad119f93947bb8
Felix Wagner <post@felixwagner.eu>
2025-03-18 10:23:17 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// Aussagekräftige Namen für Funktionen und Variablen
+// camelCase für die Benennung
+// Code formatiert
+// Maximal 14 Zeilen pro Funktion
+// Gleicher abstand zwischen Funktionen (1 oder 2 Leerzeichen)
+// HTML Templates in extra-Funktionen ausgelagert
+// Suchleiste: Man muss mindestens 3 Buchstaben eingeben, bevor gesucht wird
