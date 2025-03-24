6c4b8cb95fdc85aaaa10c248fb753fda1d338d60
Felix Wagner <post@felixwagner.eu>
2025-03-24 15:45:59 +0100

    feat: basic design

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,14 @@
 // Gleicher abstand zwischen Funktionen (1 oder 2 Leerzeichen)
 // HTML Templates in extra-Funktionen ausgelagert
 // Suchleiste: Man muss mindestens 3 Buchstaben eingeben, bevor gesucht wird
+let baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+async function init() {}
+
+async function getPokemonData(id) {}
+
+function renderCards() {}
+
+function getCardTemplate(id) {}
+
+// Bilder sind unter sprites.other.home.front_default zu finden.
